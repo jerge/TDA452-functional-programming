4fea06f940a2b94c01db001f19bb437b840c79ac
Jacob Pedersen <jacobsqy@teknik.io>
2018-12-11 14:25:04 +0100

    iuhseg

--- Lab4/IO.hs
+++ Lab4/IO.hs
@@ -8,11 +8,28 @@ printMinefield :: Minefield -> IO ()
 printMinefield (Minefield ms) = mapM_ printRow ms
 
 printRow :: [Tile] -> IO ()
-printRow ms = putStrLn (map tileToChar (ms))
+printRow ms = putStrLn (map tileToChar ms)
 
 tileToChar :: Tile -> Char
 tileToChar Unknown = '#'
 tileToChar Mine = 'm'
 tileToChar Empty = '0'
+tileToChar (Num 0) = '0'
 tileToChar (Num i) = intToDigit i
 tileToChar Flag = 'X'
+
+play :: IO ()
+play = do print exampleMinefield
+          putStrLn "Enter a coordinate to select it"
+          putStr "Enter row: "
+          putStr "Enter col: "
+          play
+          
+
+
+showTheDifference :: IO ()
+showTheDifference = do putStrLn "Enter two numbers:"
+                       x <- readLn
+                       y <- readLn
+                       putStr "The difference is: "
+                       print (x-y)
